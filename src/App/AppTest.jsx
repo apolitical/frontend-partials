e82c05353685dcda359a9c8d61090031fab7c1f0
Daniel Mason <daniel.mason@apolitical.co>
2018-03-06 14:41:44 +0000

    Improved test

--- src/App/AppTest.jsx
+++ src/App/AppTest.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from './App';
 
 describe('App', () => {
   it('should default to "Hello World"', () => {
     const app = shallow(<App />);
     expect(app.text()).to.equal('Hello World');
+  });
+
+  it('should accept a name', () => {
+    const app = shallow(<App name="Apolitical" />);
+    expect(app.text()).to.equal('Hello Apolitical');
   });
 });

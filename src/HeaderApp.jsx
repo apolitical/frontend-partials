2eca27812600d9aebb6a999e6f3fc30c11400127
Daniel Mason <daniel.mason@apolitical.co>
2019-01-09 12:40:11 +0000

    Added: HeaderContainer with state management

--- src/HeaderApp.jsx
+++ src/HeaderApp.jsx
@@ -1,21 +1,20 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Organism } from '@apolitical/styleguide';
 import { ThemeProvider } from 'styled-components';
 
 import { meReducer } from './App/Reducers/Member/MeReducer';
-// import Header from './App/Component/Organism/Header/HeaderContainer';
+import Header from './App/Component/Organism/Header/HeaderContainer';
 import ApoliticalBrand from './App/Theme/ApoliticalBrand';
 import ApoliticalGlobalStyles from './App/Theme/ApoliticalGlobalStyles';
 import { headerReducer } from './App/Component/Organism/Header/HeaderReducer';
 
 import '@babel/polyfill';
-
-const { Header } = Organism;
+import type { Member } from './types';
+import { getMember } from './App/Api/api';
 
 const reducers = combineReducers({
   meReducer,
@@ -23,16 +22,39 @@ const reducers = combineReducers({
 });
 const store = createStore(reducers);
 
-const HeaderApp = () => (
-  <Provider store={store}>
-    <ApoliticalGlobalStyles />
-    <BrowserRouter>
-      <ThemeProvider theme={ApoliticalBrand}>
-        <Header />
-      </ThemeProvider>
-    </BrowserRouter>
-  </Provider>
-);
+type Props = {}
+
+type State = {
+  me: ?Member,
+}
+
+class HeaderApp extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      me: null,
+    };
+  }
+
+  componentWillMount() {
+    getMember('me')
+      .then((me) => this.setState({ me }))
+      .catch(() => { window.location = '/login'; });
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <ApoliticalGlobalStyles />
+        <BrowserRouter>
+          <ThemeProvider theme={ApoliticalBrand}>
+            <Header {...this.state} />
+          </ThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
 
 const reactAppElement = document.getElementById('header-app');
 
